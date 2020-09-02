de1c529519c380556a73b7aeef87678c530da829
saikumar1122 <70658858+saikumar1122@users.noreply.github.com>
2020-09-03 03:27:05 +0530

    Add files via upload

---
+++ Day2/Day2 Data types.py
@@ -0,0 +1,56 @@
+#LIST and its functions 
+lis=[1,"r",,"sai","Python"]
+
+
+lis.count(1)
+lis.append("Hi LetUpgrade")
+lis.pop()
+lis.remove(1.2)
+lis.reverse()
+
+
+#Dictionary and its function
+dic={
+    "name":"Sai",
+    "age":22,
+    "City":"Hyd",
+    "Sal":"80000"
+}
+
+dic.keys()
+dic.items()
+dic.values()
+dic.get("name")
+dic.clear()
+
+
+#SETS and its finction
+sett={1,3,3,4,12,1,3}
+
+
+sett.add(55)
+sett.discard(3)
+sett.pop()
+sett.remove(55)
+sett1=sett.copy()
+
+
+#Tupple and its function
+tup1 = (1, 2,2, 3, 4, 5 );
+
+x=tup1.count(2);
+y=tup1.index(5);
+len(tup1)
+min(tup1)
+max(tup1)
+
+
+
+#String and its methods
+str="LetsUpgrade is the best Number 1 platform to learn"
+
+str.split(" ")
+str.upper()
+str.isdigit()
+str.lower()
+str.capitalize()
